b94ec14337f10a3d83c2d448b690b3486bc6182a
Maxime Thomas <thomas.max6016@gmail.com>
2018-12-23 14:18:34 +0100

    First application

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo';
+import { AppRegistry, Image } from 'react-native';
+import { Dimensions } from 'react-native'
+
+
+class BackgroundGradient extends React.Component {
+  render() {
+    return (
+      <Image
+        style={{ width: Dimensions.get('window').width / 1.5 }}
+        resizeMode={'contain'}
+        source={require('./pictures/Break_the_zone_3.png')}
+      />
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1, alignSelf: "center", alignItems: 'center', justifyContent: 'center' }}>
+        <LinearGradient
+          start={{ x: 0.0, y: -0.35 }} end={{ x: 0.75, y: 1.0 }}
+          //   locations={[0,0.5,0.6]}
+          colors={['#e60099', '#0099cc']}
+          //  colors={['#ff3300', '#ffff00']}
+          style={styles.gradient}>
+          <BackgroundGradient />
+        </LinearGradient>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  gradient: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
