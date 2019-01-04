86c1486b326f8c8efb1f67a93ac4efcb622fa854
Maxime Thomas <thomas.max6016@gmail.com>
2019-01-05 00:18:27 +0100

    Add button on the instruction page

--- navigation/Introduction.js
+++ navigation/Introduction.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {
 	StyleSheet,
-    View,
-    Text,
+	View,
+	Text,
 	Dimensions,
+	TouchableWithoutFeedback,
 } from "react-native";
 import { LinearGradient } from 'expo';
 
+import { strings } from '../locales/i18n';
 import HomeScreen from './HomeScreen';
 
 export default class Introduction extends React.Component {
@@ -22,9 +24,19 @@ export default class Introduction extends React.Component {
 					colors={['#e60099', '#0099cc']}
 					style={styles.gradient}>
 					<View style={styles.container}>
-                        <Text style={styles.text_intro}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget porta dui. Proin bibendum quis magna ut maximus. Nam luctus turpis quam, at gravida arcu convallis eget. Morbi vitae sollicitudin mauris. Ut ut dolor dictum, hendrerit libero in, eleifend massa. Nulla aliquam augue ut enim faucibus, nec congue erat dapibus. Aenean aliquam vitae dui ac mollis. Integer accumsan ut ipsum in laoreet. In eget turpis finibus, rhoncus ex a, dictum orci. Etiam urna eros, porta quis maximus sagittis, imperdiet in mauris. In hac habitasse platea dictumst. Proin non eros eget.
+						<Text style={styles.text_intro}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget porta dui. Proin bibendum quis magna ut maximus. Nam luctus turpis quam, at gravida arcu convallis eget. Morbi vitae sollicitudin mauris. Ut ut dolor dictum, hendrerit libero in, eleifend massa. Nulla aliquam augue ut enim faucibus, nec congue erat dapibus. Aenean aliquam vitae dui ac mollis. Integer accumsan ut ipsum in laoreet. In eget turpis finibus, rhoncus ex a, dictum orci. Etiam urna eros, porta quis maximus sagittis, imperdiet in mauris. In hac habitasse platea dictumst. Proin non eros eget.
                         </Text>
+					</View>
+					<View style={styles.button_container}>
+						<TouchableWithoutFeedback
+							onPress={() => this.props.navigation.navigate('Home')}>
+							<View style={styles.button}>
+								<Text style={styles.text_intro}>
+									{strings('intro.next')}
+								</Text>
+							</View>
+						</TouchableWithoutFeedback>
 					</View>
 				</LinearGradient>
 			</View>
@@ -37,11 +49,18 @@ const styles = StyleSheet.create({
 		width: Dimensions.get('window').width,
 		height: Dimensions.get('window').height,
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'space-evenly',
 	},
 	container: {
 		flex: 0.7,
 		margin: 70,
+		backgroundColor: '#6bb9f0',
+		borderRadius: 30,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	button_container: {
+		flex: 0.1,
 		backgroundColor: '#6bb9f0',
 		borderRadius: 30,
 		alignItems: 'center',
@@ -54,5 +73,13 @@ const styles = StyleSheet.create({
 		color: 'white',
 		margin: 30,
 		textAlign: 'justify',	// doesn't exist on Android
+	},
+	button: {
+		flex: 1,
+		margin: 30,
+		backgroundColor: '#6bb9f0',
+		borderRadius: 30,
+		alignItems: 'center',
+		justifyContent: 'center',
 	},
 });
