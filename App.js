9c1f1aca8e367ca63130e7f97d9aacbe4643cdba
Maxime Thomas <thomas.max6016@gmail.com>
2019-01-04 22:07:31 +0100

    Add a splash screen

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import SplashScreen from './navigation/SplashScreen';
 import HomeScreen from './navigation/HomeScreen';
 import Challenge from './navigation/Challenge';
 import SuccessList from './navigation/SuccessList';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 const AppNavigator = createStackNavigator({
+	Splash: {
+		screen: SplashScreen,
+	},
 	Home: {
 		screen: HomeScreen,
 	},
@@ -17,7 +21,7 @@ const AppNavigator = createStackNavigator({
 	}
 },
 	{
-	  initialRouteName: 'Home',
+	  initialRouteName: 'Splash',
 	}
 );
 
---
+++ navigation/SplashScreen.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import {
+	StyleSheet,
+	View,
+	Image,
+	Dimensions,
+} from "react-native";
+import { LinearGradient } from 'expo';
+
+import Introduction from './Introduction';
+
+export default class SplashScreen extends React.Component {
+	constructor(props) {
+		super(props)
+		setTimeout(() => {this.props.navigation.navigate('Home')}, 2000)
+	}
+
+	static navigationOptions = {
+		header: null,
+	};
+
+	render() {
+		return (
+			<View style={{ flex: 1, alignSelf: "center", alignItems: 'center', justifyContent: 'center' }}>
+				<LinearGradient
+					start={{ x: 0.0, y: -0.35 }} end={{ x: 0.75, y: 1.0 }}
+					colors={['#e60099', '#0099cc']}
+					style={styles.gradient}>
+					<View style={styles.imageContainer}>
+						<Image style={styles.img} source={require('../pictures/Break_the_zone_3.png')} />
+					</View>
+				</LinearGradient>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	gradient: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	imageContainer: {
+		flex: 1,
+		justifyContent: 'center',
+	},
+	img: {
+		flex: 1,
+		resizeMode: 'contain',
+		width: Dimensions.get('window').width / 1.5,
+		overflow: 'visible',
+	},
+});
